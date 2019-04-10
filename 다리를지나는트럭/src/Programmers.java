95effcf501078afacc5d052104fb7a7beea4aa41
parkjunghun <go9018@naver.com>
2019-04-10 23:15:19 +0900

    [2019.04.10]

---
+++ 다리를지나는트럭/src/Programmers.java
@@ -0,0 +1,14 @@
+public class Programmers {
+
+    public static void main(String[] args) {
+        int[] weight = {7, 4, 5, 6};
+        new Solution().solution(2,10, weight);//return ->8
+    }
+}
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
