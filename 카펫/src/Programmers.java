08a5c67037ffe810f25a7ba63b04ebf9d4e129a1
parkjunghun <go9018@naver.com>
2019-04-18 23:43:27 +0900

    [2019.04.18]

--- 카펫/src/Programmers.java
+++ 카펫/src/Programmers.java
@@ -1,15 +1,41 @@
 public class Programmers {
 
     public static void main(String[] args) {
-        new Solution().solution(10, 2);// [4,3]
-        new Solution().solution(8, 1);// [3,3]
-        new Solution().solution(24, 24);// [8,6]
+//        new Solution().solution(10, 2);// [4,3]
+//        new Solution().solution(8, 1);// [3,3]
+//        new Solution().solution(24, 24);// [8,6]
+        new Solution().solution(12,3); // [5,3]
+        new Solution().solution(12,4); // [4,4]
     }
 }
 
 class Solution {
     public int[] solution(int brown, int red) {
         int[] answer = {};
+        int brownWidth;
+        int brownHeight;
+
+        if (red == 1) {
+            return new int[]{3, 3};
+        } else if (red == 2){
+            return new int[]{4,3};
+        }
+        else{
+            int height;
+            for (int i = 1; i <= red; i++) {
+                height = red / i;
+                if (i * height == red && i > height || i == height) {
+                    System.out.println("width->" + i + "height->" + height);
+                    brownWidth = 2 * (i + 2);
+                    brownHeight = 2 * height;
+                    System.out.println(brownWidth+","+brownHeight);
+                    if (brownHeight + brownWidth == brown) {
+                        System.out.println("가로" + brownWidth + "세로" + brownHeight);
+                        return new int[]{i+2,height+2};
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
