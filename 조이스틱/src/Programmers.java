662fcaf9372a1d5cf3b9ce42bfaa4ae52e9fcb9c
parkjunghun <go9018@naver.com>
2019-04-18 18:31:59 +0900

    [2019.04.18]

--- 조이스틱/src/Programmers.java
+++ 조이스틱/src/Programmers.java
@@ -1,73 +1,55 @@
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Programmers {
 
     public static void main(String[] args) {
-//        new Solution().solution("JEROEN"); //return 56
-//        new Solution().solution("JAN"); //return 23
-//        new Solution().solution("JAZ"); // return 11
+        new Solution().solution("JEROEN"); //return 56
+        new Solution().solution("JAN"); //return 23
+        new Solution().solution("JBZ"); // return 11
 //        new Solution().solution("ABBDEC"); // return 16
 //        new Solution().solution("BAAAB"); // return 6
-        new Solution().solution("ABABAAAAAAABA "); // return 10
+//        new Solution().solution("ABABAAAAAAABA"); // return 10
     }
 }
 
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        StringBuilder sb = new StringBuilder();
         int nameLength = name.length();
-
-        for (int i = 0; i < nameLength; i++) {
-            sb.append("A");
-        }
-        System.out.println(sb.toString());
 
         int[] compareArray = new int[nameLength];
         Arrays.fill(compareArray, 0);
+
         int[] indexArray = new int[nameLength];
         for (int i = 0; i < indexArray.length; i++) {
             indexArray[i] = name.charAt(i) - 'A';
         }
 
-        int sum = 0;
         for (int i = 0; i < indexArray.length; i++) {
-            sum += indexArray[i];
             if (indexArray[i] > 13) {
                 indexArray[i] = 25 - indexArray[i] + 1;
             }
         }
-        if (sum == 0) {
-            return 0;
-        } else {
-            int zeroCount = 0;
-            int index = 0;
-            while (!Arrays.equals(compareArray, indexArray)) {
-                if (indexArray[index] == compareArray[index]) {
-                    if (indexArray[index] == 0) {
-                        if (index != 0){
-                            zeroCount++;
-                        }
-                        index++;
-                        answer++;
-                    } else {
-                        index++;
-                        answer++;
-                    }
-                } else {
-                    if (indexArray[index] == 0) {
-                        index++;
-                        zeroCount++;
-                    } else {//0이 아닐떄
-                        compareArray[index] += 1;
-                        answer++;
-                    }
-                }
-                System.out.println(Arrays.toString(compareArray));
+
+        int index = 0;
+        while (!Arrays.equals(indexArray, compareArray)){
+
+            if (indexArray[index] != compareArray[index]){
+                compareArray[index]++;
+                answer++;
+            }else if (indexArray[index] == compareArray[index]){
+                index++;
+                answer++;
             }
-            answer = answer - zeroCount;
-            System.out.println(answer);
+
+            System.out.println(Arrays.toString(compareArray));
         }
+
+
         return answer;
     }
 }
---
+++ 카펫/src/Programmers.java
@@ -0,0 +1,15 @@
+public class Programmers {
+
+    public static void main(String[] args) {
+        new Solution().solution(10, 2);// [4,3]
+        new Solution().solution(8, 1);// [3,3]
+        new Solution().solution(24, 24);// [8,6]
+    }
+}
+
+class Solution {
+    public int[] solution(int brown, int red) {
+        int[] answer = {};
+        return answer;
+    }
+}
