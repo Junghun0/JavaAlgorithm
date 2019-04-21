e08705bc6eab3521b46e0861cb06fa907e97a3a9
parkjunghun <go9018@naver.com>
2019-04-21 15:59:17 +0900

    [2019.04.21]

--- 영어끝말잇기/src/Programmers.java
+++ 영어끝말잇기/src/Programmers.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Programmers {
 
@@ -12,38 +10,42 @@ public class Programmers {
 //        new Solution().solution(2, testcase2); // [1,3]
 //
         String[] testcase3 = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
-        new Solution().solution(3,testcase3); // [3,3]
+        new Solution().solution(3, testcase3); // [3,3]
 
         String[] testcase4 = {"tank", "sex", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
-        new Solution().solution(2,testcase4); // [3,3]
+        new Solution().solution(2, testcase4); // [3,3]
     }
 }
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
-        List<String> wordList = new ArrayList<>();
-        int nextWordsIndex = 1;
-        int playerLength = 1;
-        int resultGameTurn = 1;
-        int playerIndex = 1;
+        int[] answer = new int[2];
+        Map<String, Integer> checkList = new HashMap<>();
+        checkList.put(words[0], 0);
+        for(int i=0;i<words.length-1;i++){
 
-        for (int i = 0; i < words.length - 1; i++) {
-            wordList.add(words[i]);
-            if (words[i].charAt(words[i].length() - 1) - 'a' == words[nextWordsIndex].charAt(0) - 'a' && !wordList.contains(words[nextWordsIndex])){
-                playerLength++;
-                playerIndex++;
-                if (playerLength%n == 0){
-                    resultGameTurn++;
-                    playerIndex = 1;
+            String x =words[i].substring(words[i].length()-1, words[i].length());
+            String y = words[i+1].substring(0, 1);
+
+            if(!x.equals(y) || checkList.containsKey(words[i+1])){
+
+                if((i+2)%n==0){
+                    answer[0] = n;
+                    answer[1] = (i+2)/n;
+                    return answer;
+                }else{
+                    answer[0] = (i+2)%n;
+                    answer[1] =    ((i+2)/n)+1;
+                    return answer;
                 }
-            }else if (wordList.contains(words[nextWordsIndex])){//이미 사용된 단어!
-                return new int[]{playerIndex,resultGameTurn};
-            }else if (words[i].charAt(words[i].length() - 1) - 'a' != words[nextWordsIndex].charAt(0) - 'a'){
-                return new int[]{playerIndex,resultGameTurn};
+
             }
-            nextWordsIndex++;
+            else{
+                checkList.put(words[i+1], i+1);
+                answer[0]=0;
+                answer[1]=0;
+            }
         }
-        return new int[]{0,0};
+        return answer;
     }
 }
---
+++ 예상대진표/src/Programmers.java
@@ -0,0 +1,12 @@
+public class Programmers {
+    public static void main(String[] args) {
+        new Solution().solution(8, 4, 7); //return 3
+    }
+}
+
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 3;
+        return answer;
+    }
+}
