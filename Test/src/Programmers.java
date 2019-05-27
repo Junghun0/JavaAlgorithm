dc962e2bdfa41cc4e38d7a1d652237db0a255594
parkjunghoon <go9018@naver.com>
2019-05-27 16:01:06 +0900

    [2019.05.27]

--- Test/src/Programmers.java
+++ Test/src/Programmers.java
@@ -1,10 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programmers {
 
     public static void main(String[] args) {
-        String[] testcase = {"Jane", "Kim"};
-        new Solution().solution(testcase);
+//        String[] testcase = {"Jane", "Kim"};
+//        new Solution().solution(testcase);
+        Scanner scanner = new Scanner(System.in);
+        int kindCount = scanner.nextInt();
+        int totalMoney = scanner.nextInt();
+        int coin [] = new int[kindCount];
+        int maxtmp = 0;
+        int answer = 0;
+
+        for (int i = 0; i < kindCount; i++) {
+            coin[i] = scanner.nextInt();
+            if (coin[i] <= totalMoney) maxtmp = i;
+        }
+
+        for (int i = maxtmp; i >= 0 ; i--) {
+            answer += totalMoney / coin[i];
+            totalMoney %= coin[i];
+        }
+        System.out.println(answer);
     }
 }
 
