a2025fe042f67285b8145d13ef47d2366b3ea0e7
parkjunghoon <go9018@naver.com>
2019-07-02 19:09:53 +0900

    [2019.07.02]

--- Test/src/Programmers.java
+++ Test/src/Programmers.java
@@ -1,42 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Programmers {
 
     public static void main(String[] args) {
 //        String[] testcase = {"Jane", "Kim"};
 //        new Solution().solution(testcase);
-        Scanner scanner = new Scanner(System.in);
-        int kindCount = scanner.nextInt();
-        int totalMoney = scanner.nextInt();
-        int coin [] = new int[kindCount];
-        int maxtmp = 0;
-        int answer = 0;
-
-        for (int i = 0; i < kindCount; i++) {
-            coin[i] = scanner.nextInt();
-            if (coin[i] <= totalMoney) maxtmp = i;
-        }
-
-        for (int i = maxtmp; i >= 0 ; i--) {
-            answer += totalMoney / coin[i];
-            totalMoney %= coin[i];
-        }
-        System.out.println(answer);
+        new Solution().solution("PM 01:00:00","AM 01:00:00"); //-> 12:00:00
+        new Solution().solution("AM 00:00:01","AM 00:00:02"); //-> 00:00:01
+        new Solution().solution("AM 00:00:02","AM 00:00:01");
+        new Solution().solution("AM 04:00:02","PM 04:00:01");
     }
 }
+
 
 class Solution {
-    public String solution(String[] seoul) {
-        final String answerStr = "김서방은 ";
-        final String answerStr2 = "에 있다";
+    public String solution(String start, String end) {
         String answer = "";
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")){
-                answer = answerStr + i+ answerStr2;
-            }
+        String startAMPM = start.substring(0,2);
+        String endAMPM = end.substring(0,2);
+
+        String startTime = start.substring(3, start.length());
+        String endTime = end.substring(3, end.length());
+
+        String[] startTimeArray = startTime.split(":");
+        String[] endTimeArray = endTime.split(":");
+
+        int startSecond = hourToSecond(Integer.parseInt(startTimeArray[0]),Integer.parseInt(startTimeArray[1]),Integer.parseInt(startTimeArray[2]));
+        int endSecond = hourToSecond(Integer.parseInt(endTimeArray[0]), Integer.parseInt(endTimeArray[1]),Integer.parseInt(endTimeArray[2]));
+
+        if (startAMPM.equals(endAMPM)){
+            answer = secondToAnswer(endSecond - startSecond);
+        }else{
+            answer = secondToAnswer((endSecond + 43200) - startSecond);
         }
+        System.out.println(answer);
         return answer;
     }
+
+    public static String secondToAnswer(int second){
+        return String.format("%02d:%02d:%02d",second/3600,second%3600 / 60,second % 3600 % 60);
+    }
+
+    public static int hourToSecond(int hour, int min, int second){
+        int hourToSecond = hour * 60 * 60;
+        int minToSecond = min * 60;
+        return hourToSecond + minToSecond + second;
+    }
 }
+//
+//class Solution {
+//    public String solution(String[] seoul) {
+//        final String answerStr = "김서방은 ";
+//        final String answerStr2 = "에 있다";
+//        String answer = "";
+//        for (int i = 0; i < seoul.length; i++) {
+//            if (seoul[i].equals("Kim")){
+//                answer = answerStr + i+ answerStr2;
+//            }
+//        }
+//        return answer;
+//    }
+//}
---
+++ Test2/src/Test2.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        /*
+        [1, 4, 2], [5, 4, 4]
+        기댓값 〉	29
+
+       	[1, 2], [3, 4]
+        기댓값 〉	10
+        */
+
+//        new Solution().solution(new int[]{1, 4, 2}, new int[]{5, 4, 4});
+
+        /*
+        입력값 〉	3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+        기댓값 〉	[3, 3]
+
+        입력값 〉 5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]
+        기댓값 〉	[0, 0]
+
+        입력값 〉	2, ["hello", "one", "even", "never", "now", "world", "draw"]
+        기댓값 〉	[1, 3]
+        */
+        new Solution2().solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"});
+    }
+
+    static class Solution2 {
+        List<String> wordsList = new ArrayList<>();
+        public int[] solution(int n, String[] words) {
+            int[] answer = new int[2];
+            int loopCount = 1;
+            int userIndex = 0;
+            boolean flag = true;
+
+            Collections.addAll(wordsList, words);
+
+            for (int i = 1; i < words.length; i++) {
+                userIndex++;
+                if (userIndex == n){
+                    loopCount++;
+                    userIndex = 1;
+                }
+                
+                if (checkValid(wordsList.get(i-1),wordsList.get(i)) && checkDuplicate(wordsList, wordsList.get(i), i)){
+                }else{
+                    flag = false;
+                    answer[0] = userIndex + 1;
+                    answer[1] = loopCount - 1;
+                }
+            }
+
+            if (flag){
+                answer[0] = 0;
+                answer[1] = 1;
+            }
+
+            return answer;
+        }
+
+        public boolean checkDuplicate(List<String> wordsList, String word, int index){
+            for (int i = 0; i < index; i++) {
+                if (wordsList.get(i).equals(word)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //끝말잇기 성립체크
+        public boolean checkValid(String front, String back){
+            char frontWord = front.charAt(front.length() - 1);
+            char backWord = back.charAt(0);
+            return frontWord == backWord;
+        }
+    }
+
+    static class Solution {
+        public int solution(int[] A, int[] B) {
+            int answer;
+            int[] sumArray = new int[A.length];
+            int length = A.length;
+            int index = A.length - 1;
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            for (int i = 0; i < length; i++) {
+                int min = A[i];
+                int max = B[index];
+                sumArray[i] = min * max;
+                index--;
+            }
+            answer = getSum(sumArray);
+
+            return answer;
+        }
+
+        public int getSum(int[] data) {
+            int sum = 0;
+            for (int datum : data) {
+                sum += datum;
+            }
+            return sum;
+        }
+    }
+}
