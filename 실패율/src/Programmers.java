d1ebeb7194f26cef9dca5509f34dfb2d97b68e96
parkjunghoon <go9018@naver.com>
2019-08-21 15:36:44 +0900

    [2019.08.21]

--- 실패율/src/Programmers.java
+++ 실패율/src/Programmers.java
@@ -9,6 +9,12 @@ public class Programmers {
         int[] testcase2 = {4, 4, 4, 4, 4};
         new Solution().solution(4, testcase2);
 
+        int[] testcase3 = {1};
+        new Solution().solution(7, testcase3);
+
+        int[] testcase4 = {6,6,6,6,6};
+        new Solution().solution(5, testcase4);
+
     }
 }
 
@@ -17,37 +23,44 @@ class Solution {
         int[] answer = new int[N];
 
         Arrays.sort(stages);
-        System.out.println(Arrays.toString(stages));
-        //1-> 8/1  2-> 7/3  3-> 4/2 4-> 2/1 5-> 0
+        //실패율 같으면 낮은 스테이지부터
+        // 1, 2, 2, 2, 3, 3, 4, 6
+        //1-> 1/8 , 2-> 3/7 , 3-> 2/4 , 4-> 1/2 , 5-> 0
+        //return 3,4,2,1,5
+        List<Fail> failList = new ArrayList<>();
+        List<Integer> stagesList = new ArrayList<>();
 
-        List<Integer> stageUserList = new ArrayList<>();
-        for (int onStage : stages){
-            stageUserList.add(onStage);
+        for (int stage : stages){
+            stagesList.add(stage);
         }
 
-        int stageCheck = 1;
+        int onUser;
         int index = 0;
-        int onStageUserCount = 0;
-        while (!stageUserList.isEmpty()){
 
-            if (stageUserList.get(index) == stageCheck){
-                onStageUserCount++;
-                stageUserList.remove(0);
-            }else {
-                stageUserList.remove(0);
-                index = 0;
-                stageCheck++;
+        for (int j = 1; j < N + 1; j++) {
+            onUser = 0;
+            while (!stagesList.isEmpty()){
+                if (j != stagesList.get(index)) {
+                    break;
+                }else if (j == stagesList.get(index)){
+                    onUser++;
+                    stagesList.remove(0);
+                }
             }
-
-            System.out.println("onstageUsercount-> "+onStageUserCount + " stageCheck-> "+stageCheck + " List size-> "+stageUserList.size()+" onStageUserCount-> "+onStageUserCount);
+            if (onUser == 0){
+                failList.add(new Fail(j, (double)0));
+            }else{
+                failList.add(new Fail(j, ((double)onUser / (double)(stagesList.size() + onUser))));
+            }
         }
 
+        Collections.sort(failList);
+        for (int i = 0; i < failList.size(); i++) {
+            answer[i] = failList.get(i).stage;
+        }
+        System.out.println();
 
         return answer;
-    }
-
-    private double makeFailrate(double under, double up){
-        return Double.parseDouble(String.format("%.3f",up/under));
     }
 }
 
@@ -55,31 +68,20 @@ class Fail implements Comparable<Fail>{
     public int stage;
     public double failRate;
 
-    public int getStage() {
-        return stage;
-    }
-
-    public void setStage(int stage) {
+    public Fail(int stage, double failRate) {
         this.stage = stage;
-    }
-
-    public double getFailRate() {
-        return failRate;
-    }
-
-    public void setFailRate(double failRate) {
         this.failRate = failRate;
     }
 
     @Override
     public int compareTo(Fail o) {
-        if (this.stage == o.stage){
-            if (this.failRate < o.failRate){
-                return -1;
+        if (this.failRate < o.failRate){
+            if (this.stage > o.stage){
+                return 1;
             }
-        }else{
-            if (this.failRate < o.failRate){
-                return -1;
+        }else if (this.failRate == o.failRate){
+            if (this.stage > o.stage){
+                return 1;
             }
         }
         return -1;
